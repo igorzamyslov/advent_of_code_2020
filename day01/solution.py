0b96077af8432d4b2cfdcde6abc9e3bb754d96c8
Igor Zamyslov <igor.zamyslov@cognostics.de>
2020-12-01 20:08:21 +0100

    Solution for day01 puzzle

---
+++ day01/solution.py
@@ -0,0 +1,24 @@
+import itertools
+import operator
+from functools import reduce
+from typing import List
+
+
+def read_input():
+    with open("day01/input.txt") as f:
+        data = list(map(int, f.readlines()))
+    return data
+
+
+def find_answer(input_list: List, combination_length: int):
+    for combination in itertools.combinations(input_list, combination_length):
+        if sum(combination) == 2020:
+            return reduce(operator.mul, combination, 1)
+    raise ValueError
+
+
+if __name__ == "__main__":
+    # part 1
+    print(find_answer(read_input(), 2))
+    # part 2
+    print(find_answer(read_input(), 3))
