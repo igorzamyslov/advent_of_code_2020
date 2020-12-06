016089d3f1f9dc4a2ee21490e617c89fffff724f
Igor Zamyslov <zamyslovigor@gmail.com>
2020-12-06 11:51:56 +0100

    Optimise and restructure the solution

--- day06/solution.py
+++ day06/solution.py
@@ -1,21 +1,23 @@
-from typing import List, Tuple
+from typing import List
 
-def get_answers() -> List[Tuple[str, ...]]:
+def get_groups() -> List[List[str]]:
     with open("input.txt") as f:
-        return [tuple(person for person in group.split("\n")) 
-                for group in f.read().split("\n\n")]
+        return [group.split("\n") for group in f.read().split("\n\n")]
 
-def count_unique_answers_per_group() -> int:
-    return sum(len(set("".join(group)))
-                   for group in get_answers())
+def count_unique_answers_per_group(groups) -> int:
+    return sum(len(set("".join(group))) for group in groups)
 
-def count_common_answers_per_group() -> int:
+def count_common_answers_per_group(groups) -> int:
     return sum(len(set.intersection(*(set(person) for person in group)))
-               for group in get_answers())
+               for group in groups)
+
+def main():
+    input_groups = get_groups()
+    # part 1
+    print(count_unique_answers_per_group(input_groups))
+    # part 2
+    print(count_common_answers_per_group(input_groups))
 
 
 if __name__ == "__main__":
-    # part 1
-    print(count_unique_answers_per_group())
-    # part 2
-    print(count_common_answers_per_group())
+    main()
