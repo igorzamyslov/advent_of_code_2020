0aa81fb761d60c269ad1bf0f73b83a08f890ff6a
Igor Zamyslov <igor.zamyslov@cognostics.de>
2020-12-07 17:43:05 +0100

    Optimise day07 a bit

--- day07/solution.py
+++ day07/solution.py
@@ -1,18 +1,18 @@
 import re
-from typing import List, Tuple, Dict, Set
+from typing import Tuple, Dict, Set
 
 
-ContainedColourInfoType = Tuple[str, int]
-ColoursInfoType = Dict[str, List[ContainedColourInfoType]]
+ContainedColoursInfoType = Dict[str, int]
+ColoursInfoType = Dict[str, Dict[str, int]]
 
 
-def _parse_line(line: str) -> Tuple[str, List[ContainedColourInfoType]]:
+def _parse_line(line: str) -> Tuple[str, ContainedColoursInfoType]:
     colour, colour_contains_string = line.split(" bags contain ")
-    colour_contains_list = []
+    contained_colours_info = {}
     for match in re.finditer(r"(\d+?) (.+?) bags?[,\.]", colour_contains_string):
         count, contained_colour = match.groups()
-        colour_contains_list.append((contained_colour, int(count)))
-    return colour, colour_contains_list
+        contained_colours_info[contained_colour] = int(count)
+    return colour, contained_colours_info
 
 
 def get_parsed_lines() -> ColoursInfoType:
@@ -23,16 +23,15 @@ def get_parsed_lines() -> ColoursInfoType:
 def get_containing_colours(colour: str, colours_info: ColoursInfoType) -> Set[str]:
     containing_colours = set()
     for containing_colour, contained_colours in colours_info.items():
-        if any(contained_colour == colour for contained_colour, _ in contained_colours):
+        if colour in contained_colours:
             containing_colours.add(containing_colour)
             containing_colours.update(get_containing_colours(containing_colour, colours_info))
     return containing_colours
 
 
 def count_contained_bags(colour: str, colours_info: ColoursInfoType, multiplier: int = 1) -> int:
-    return sum(count * multiplier + count_contained_bags(c_colour, colours_info,
-                                                         multiplier * count)
-               for c_colour, count in colours_info[colour])
+    return sum(n * multiplier + count_contained_bags(c, colours_info, multiplier * n)
+               for c, n in colours_info[colour].items())
 
 
 def main():
